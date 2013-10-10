e1da489803c775c40f56f08fbef90cae347372f2
wdlindmeier <wdlindmeier@gmail.com>
2013-10-10 19:07:27 -0400

    Added some niceness to the timeline editor

--- code/BigScreensShared/Headers/SharedTypes.hpp
+++ code/BigScreensShared/Headers/SharedTypes.hpp
@@ -10,6 +10,7 @@
 
 #include "cinder/Cinder.h"
 #include "cinder/Rect.h"
+#include "cinder/gl/gl.h"
 
 namespace bigscreens
 {
@@ -44,4 +45,71 @@ namespace bigscreens
         {
         };
     };
+    
+    struct Slider
+    {
+        Slider(ci::Rectf rect) : mRect(rect), mValue(0){};
+        Slider(){};
+        ~Slider(){};
+        
+        ci::Rectf mRect;
+        float mValue;
+        bool mIsActive;
+        
+        void update(ci::Vec2f mousePosition)
+        {
+            ci::Vec2f relPos = mousePosition - mRect.getUpperLeft();
+            float scalarVal = relPos.x / mRect.getWidth();
+            mValue = std::min<float>(1.0f, std::max<float>(0.0f, scalarVal));
+        }
+        
+        bool contains(ci::Vec2f position)
+        {
+            return mRect.contains(position);
+        }
+        
+        float getValue()
+        {
+            return mValue;
+        }
+        
+        void setValue(const float value)
+        {
+            mValue = value;
+        }
+        
+        void setIsActive(bool isActive)
+        {
+            mIsActive = isActive;
+        }
+        
+        bool getIsActive()
+        {
+            return mIsActive;
+        }
+        
+        void render(bool isEnabled)
+        {
+            ci::gl::lineWidth(1.0f);
+            ci::gl::color(ci::Color::white());
+            ci::gl::drawLine(ci::Vec2f(mRect.x1, mRect.getCenter().y),
+                         ci::Vec2f(mRect.x2, mRect.getCenter().y));
+            ci::gl::lineWidth(6.0f);
+            if (isEnabled)
+            {
+                ci::gl::color(ci::ColorAf(1.0f, 0, 0, 1.0f));
+            }
+            else
+            {
+                ci::gl::color(ci::ColorAf(0.5f, 0.5f, 0.5f, 1.0f));
+            }
+            float offsetX = mRect.getWidth() * mValue;
+            ci::gl::drawLine(ci::Vec2f(mRect.x1 + offsetX,
+                                       mRect.y1),
+                             ci::Vec2f(mRect.x1 + offsetX,
+                                       mRect.y2));
+        }
+    };
+    
+
 }
--- code/GridMaker/src/TimeLineEditorApp.cpp
+++ code/GridMaker/src/TimeLineEditorApp.cpp
@@ -16,6 +16,9 @@ using namespace std;
 using namespace bigscreens;
 
 const static float kTimelineDurationMinutes = 4.0f;
+const static long long kMsFullDuration = kTimelineDurationMinutes * 60 * 1000;
+const static long kRangeTransition = 20000; // 20 seconds
+const static long long kRangePosition = kMsFullDuration;
 
 // See misc
 static double millisecPerPixel();
@@ -40,6 +43,7 @@ public:
     void mouseUp(MouseEvent event);
     void mouseDrag(MouseEvent event);
     void mouseMove(MouseEvent event);
+    void adjustSelectedLayoutTimestamp(long long timeDelta);
     
     // Key
     void keyDown(KeyEvent event);
@@ -48,14 +52,22 @@ public:
     // App Loop
     void update();
     void draw();
+    void renderSliders();
     
     // Misc
     Rectf rectForLayout(int layoutIndex);
+    void selectLayout(int layoutIndex);
+    void updateSlidersForSelectedLayout();
     
     // Member vars
     vector<GridLayout> mGridLayouts;
     int mIdxSelectedLayout;
     Vec2f mMousePosition;
+    bool mIsShiftDown;
+    
+    Slider mSliderPosition;
+    Slider mSliderTransition;
+    
 };
 
 
@@ -69,6 +81,22 @@ void TimeLineEditorApp::prepareSettings(Settings *settings)
 
 void TimeLineEditorApp::setup()
 {
+    mMousePosition = Vec2f::zero();
+    mIsShiftDown = false;
+    float sliderWidth = (getWindowWidth() * 0.5) - 300.f;
+    float sliderOffsetX = 120.0f;
+    float sliderOffsetY = 10.0f;
+    float sliderHeight = 20.0f;
+    float xDuration = sliderOffsetX;
+    mSliderPosition = Slider(Rectf(xDuration,
+                                   sliderOffsetY,
+                                   xDuration + sliderWidth,
+                                   sliderOffsetY + sliderHeight));
+    float xTransition = (getWindowWidth() * 0.5f) + sliderOffsetX;
+    mSliderTransition = Slider(Rectf(xTransition,
+                                     sliderOffsetY,
+                                     xTransition + sliderWidth,
+                                     sliderOffsetY + sliderHeight));
     reload();
 }
 
@@ -105,7 +133,7 @@ void TimeLineEditorApp::loadAllGrids()
 
 void TimeLineEditorApp::reload()
 {
-    mIdxSelectedLayout = -1;
+    selectLayout(-1);
     mGridLayouts.clear();
     loadAllGrids();
 }
@@ -115,41 +143,106 @@ void TimeLineEditorApp::reload()
 void TimeLineEditorApp::mouseDown( MouseEvent event )
 {
     mMousePosition = event.getPos();
-    
-    int numLayouts = mGridLayouts.size();
-    for (int i = 0; i < numLayouts; ++i)
+
+    if (mIdxSelectedLayout != -1)
     {
-        Rectf layoutRect = rectForLayout(i);
-        if (layoutRect.contains(event.getPos()))
+        mSliderPosition.setIsActive(mSliderPosition.contains(mMousePosition));
+        mSliderTransition.setIsActive(mSliderTransition.contains(mMousePosition));
+    }
+
+    // Only select if we didn't click into a slider.
+    // If we did, maintain the previous selection.
+    if (!mSliderPosition.getIsActive() &&
+        !mSliderTransition.getIsActive())
+    {
+        selectLayout(-1);
+        
+        int numLayouts = mGridLayouts.size();
+        for (int i = 0; i < numLayouts; ++i)
         {
-            mIdxSelectedLayout = i;
-            break;
+            Rectf layoutRect = rectForLayout(i);
+            if (layoutRect.contains(event.getPos()))
+            {
+                selectLayout(i);
+                break;
+            }
         }
     }
+    
     if (mIdxSelectedLayout > -1)
     {
         console() << "Selected layout " << mGridLayouts[mIdxSelectedLayout].getName() << endl;
     }
+    
 }
 
 void TimeLineEditorApp::mouseUp(MouseEvent event)
 {
     mMousePosition = event.getPos();
+    mSliderPosition.setIsActive(false);
+    mSliderTransition.setIsActive(false);
 }
 
 void TimeLineEditorApp::mouseDrag(MouseEvent event)
 {
     Vec2f mouseDelta = event.getPos() - mMousePosition;
     mMousePosition = event.getPos();
-    
-    int timelineDelta = mouseDelta.x * millisecPerPixel();
-    if (mIdxSelectedLayout != -1)
+
+    if (mSliderPosition.getIsActive())
     {
+        mSliderPosition.update(mMousePosition);
+        assert(mIdxSelectedLayout > -1);
+        GridLayout & selectedLayout = mGridLayouts[mIdxSelectedLayout];
+        long long timestamp = mSliderPosition.getValue() * kRangePosition;
+        long long timeDelta = timestamp - selectedLayout.getTimestamp();
+        adjustSelectedLayoutTimestamp(timeDelta);
+    }
+    else if (mSliderTransition.getIsActive())
+    {
+        mSliderTransition.update(mMousePosition);
+        assert(mIdxSelectedLayout > -1);
+        GridLayout & selectedLayout = mGridLayouts[mIdxSelectedLayout];
+        long long transitionDuration = mSliderTransition.getValue() * kRangeTransition;
+        selectedLayout.setTransitionDuration(transitionDuration);
+    }
+    else if (mIdxSelectedLayout > -1)
+    {
+        long long timelineDelta = mouseDelta.x * millisecPerPixel();
+        adjustSelectedLayoutTimestamp(timelineDelta);
+    }
+}
+
+void TimeLineEditorApp::mouseMove(MouseEvent event)
+{
+    mMousePosition = event.getPos();
+}
+
+#pragma mark - Editing
+
+void TimeLineEditorApp::adjustSelectedLayoutTimestamp(long long timeDelta)
+{
+    if (mIsShiftDown)
+    {
+        // Push all of the layouts
+        int numLayouts = mGridLayouts.size();
+        for (int i = mIdxSelectedLayout; i < numLayouts; ++i)
+        {
+            GridLayout & layout = mGridLayouts[i];
+            long long timeStamp = layout.getTimestamp();
+            
+            // Update the timestamp
+            timeStamp = std::max<long long>(0, std::min<long long>(timeStamp + timeDelta, kMsFullDuration));
+            layout.setTimestamp(timeStamp);
+        }
+    }
+    else
+    {
+        // Drag the selected layout
         GridLayout & selectedLayout = mGridLayouts[mIdxSelectedLayout];
         string selectedID = selectedLayout.getUniqueID();
         long long timeStamp = selectedLayout.getTimestamp();
         // Update the timestamp
-        timeStamp += timelineDelta;
+        timeStamp = std::max<long long>(0, std::min<long long>(timeStamp + timeDelta, kMsFullDuration));
         selectedLayout.setTimestamp(timeStamp);
         
         // Resort in ascending order
@@ -162,27 +255,26 @@ void TimeLineEditorApp::mouseDrag(MouseEvent event)
             GridLayout &l = mGridLayouts[i];
             if (l.getUniqueID() == selectedID)
             {
-                mIdxSelectedLayout = i;
+                //mIdxSelectedLayout = i;
+                selectLayout(i);
                 break;
             }
         }
     }
-}
-
-void TimeLineEditorApp::mouseMove(MouseEvent event)
-{
-    mMousePosition = event.getPos();
+    updateSlidersForSelectedLayout();
 }
 
 #pragma mark - Key
 
 void TimeLineEditorApp::keyDown(KeyEvent event)
 {
-    
+    mIsShiftDown = event.isShiftDown();
 }
 
 void TimeLineEditorApp::keyUp(KeyEvent event)
 {
+    mIsShiftDown = event.isShiftDown();
+
     char key = event.getChar();
     if (key == 's')
     {
@@ -224,10 +316,16 @@ void TimeLineEditorApp::draw()
     {
         Rectf layoutRect = rectForLayout(i);
 
+        // Draw the body
         bool isSelected = i == mIdxSelectedLayout;
         if (isSelected)
         {
             gl::color(ColorAf(1.0f,1.0f,0,1.0f));
+        }
+        else if (mIsShiftDown && mIdxSelectedLayout > -1 && i > mIdxSelectedLayout)
+        {
+            // These will also be moved, so color them
+            gl::color(ColorAf(0.65f,0.8f,0,1.0f));
         }
         else
         {
@@ -235,6 +333,7 @@ void TimeLineEditorApp::draw()
         }
         gl::drawSolidRect(layoutRect);
         
+        // Draw the stroke
         if (isSelected)
         {
             gl::color(ColorAf(1.0f,1.0f,0,1.0f));
@@ -243,29 +342,75 @@ void TimeLineEditorApp::draw()
         {
             gl::color(ColorAf(0.75f,0.75f,0.75,0.5f));
         }
-        
         gl::drawStrokedRect(layoutRect);
         gl::drawString(to_string(i), Vec2f(layoutRect.x1 + 2, layoutRect.getHeight() * 0.5),
                        Color::black());
+        
+        // Draw the transition time
+        // Magenta
+        GridLayout & layout = mGridLayouts[i];
+        float transitionWidth = layout.getTransitionDuration() / millisecPerPixel();
+        Rectf transitionRect = layoutRect;
+        transitionRect.x2 = transitionRect.x1 + transitionWidth;
+        gl::color(ColorAf(1.0f,0.0f,1.0f,0.2f));
+        gl::drawSolidRect(transitionRect);
+        
     }
     
     if (mIdxSelectedLayout != -1)
     {
         GridLayout & selectedLayout = mGridLayouts[mIdxSelectedLayout];
         long long msEnter = selectedLayout.getTimestamp();
-        long long msFullDuration = kTimelineDurationMinutes * 60 * 1000;
-        long long msDuration = msFullDuration - msEnter;
+        long long msDuration = kMsFullDuration - msEnter;
         if (mIdxSelectedLayout < mGridLayouts.size() - 1)
         {
             GridLayout & nextLayout = mGridLayouts[mIdxSelectedLayout+1];
             msDuration = nextLayout.getTimestamp() - msEnter;
         }
         
-        gl::drawString(selectedLayout.getName() + " @ " +
-                       to_string(msEnter) + " l: " +
-                       to_string(msDuration),
-                       Vec2i(10, textY));
+        float seconds = msDuration / 1000.0;
+        int minutes = (int)seconds / 60;
+        if (minutes > 0)
+        {
+            seconds -= minutes * 60;
+        }
+        
+        string outp = "\"" + selectedLayout.getName() + "\" - Duration: ";
+        if (minutes > 0)
+        {
+            outp += to_string(minutes) + ":";
+        }
+        
+        char buff[100];
+        sprintf(buff, "%2.1f", seconds);
+        std::string buffAsStdStr = buff;
+        outp += buffAsStdStr;
+        
+        if (minutes == 0)
+        {
+            outp += " sec";
+        }
+        
+        gl::drawString(outp, Vec2i(10, textY));
     }
+    
+    renderSliders();
+}
+
+void TimeLineEditorApp::renderSliders()
+{
+    float labelY = 18;
+    mSliderPosition.render(mIdxSelectedLayout > -1);
+    gl::drawString("Timestamp (ms): ", Vec2f(10, labelY));
+    gl::drawString(to_string((long)(mSliderPosition.getValue() * kRangePosition)),
+                   Vec2f((getWindowWidth() * 0.5) - 150,
+                         labelY));
+
+    mSliderTransition.render(mIdxSelectedLayout > -1);
+    gl::drawString("Transition (ms): ", Vec2f((getWindowWidth() * 0.5f) + 10, labelY));
+    gl::drawString(to_string((long)(mSliderTransition.getValue() * kRangeTransition)),
+                   Vec2f(getWindowWidth() - 150,
+                         labelY));
 }
 
 #pragma mark - Misc
@@ -279,7 +424,9 @@ Rectf TimeLineEditorApp::rectForLayout(int layoutIndex)
 {
     double millisecPerPx = millisecPerPixel();
     
-    float height = getWindowHeight() - 30;
+    float vertMarginTop = 40;
+    float vertMarginBottom = 30;
+    float height = getWindowHeight() - (vertMarginBottom + vertMarginTop);
     
     int numLayouts = mGridLayouts.size();
     GridLayout & layout = mGridLayouts[layoutIndex];
@@ -290,7 +437,30 @@ Rectf TimeLineEditorApp::rectForLayout(int layoutIndex)
         GridLayout & nextLayout = mGridLayouts[layoutIndex+1];
         x2 = nextLayout.getTimestamp() / millisecPerPx;
     }
-    return Rectf(x1, 0, x2, height);
+    return Rectf(x1, vertMarginTop, x2, vertMarginTop + height);
+}
+
+void TimeLineEditorApp::selectLayout(int layoutIndex)
+{
+    mIdxSelectedLayout = layoutIndex;
+    updateSlidersForSelectedLayout();
+}
+
+void TimeLineEditorApp::updateSlidersForSelectedLayout()
+{
+    if (mIdxSelectedLayout > -1)
+    {
+        GridLayout & layout = mGridLayouts[mIdxSelectedLayout];
+        mSliderPosition.setValue((long double)layout.getTimestamp() /
+                                 (float)kRangePosition);
+        mSliderTransition.setValue((long double)layout.getTransitionDuration() /
+                                   (float)kRangeTransition);
+    }
+    else
+    {
+        mSliderPosition.setValue(0);
+        mSliderTransition.setValue(0);
+    }
 }
 
 CINDER_APP_NATIVE( TimeLineEditorApp, RendererGl )
