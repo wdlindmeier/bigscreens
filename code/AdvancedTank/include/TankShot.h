61c8daf60749605190a78d3f68cb0e5d9aaa0a66
wdlindmeier <wdlindmeier@gmail.com>
2013-11-04 12:42:55 -0500

    Added explodey goodness

--- code/AdvancedTank/include/TankShot.h
+++ code/AdvancedTank/include/TankShot.h
@@ -9,6 +9,9 @@
 #pragma once
 
 #include "cinder/Cinder.h"
+#include "cinder/gl/Vao.h"
+#include "cinder/gl/Vbo.h"
+#include "cinder/gl/GlslProg.h"
 
 namespace bigscreens
 {
@@ -19,36 +22,46 @@ namespace bigscreens
         
     public:
         
-        TankShot() :
-        mVelocity(0),
-        mTheta(0),
-        mProgress(0)
-        {};
-        
-        TankShot(float angleRadians, float velocity) :
-        mVelocity(velocity),
-        mTheta(angleRadians),
-        mProgress(0)
-        {
-        };
+        TankShot();        
+        TankShot(float angleRadians,
+                 float velocity,
+                 const ci::Vec3f & initialPosition,
+                 const ci::gl::GlslProgRef & shader);
         
         ~TankShot(){};
         
-        float getVelocity();
-        float getTheta();
-        float getProgress();
-        void  update(float amount);
-        bool  isDead();
+        float       getVelocity();
+        float       getTheta();
+        float       getProgress();
+        void        update(float amount);
+        bool        isDead();
         
-        ci::Vec2f currentProgress();
-        void  render(const ci::Vec3f & initialOffset);
+        ci::Vec2f   currentProgress();
+        void        render();
+        void        renderExplosion(ci::CameraPersp & cam);
+        void        renderMuzzleFlare(ci::CameraPersp & cam);
+        void        renderLine();
 
     private:
         
-        float     mVelocity;
-        float     mTheta;
-        float     mProgress;
-        bool      mIsDead;
+        ci::Vec2f   progressAt(float amount);
+        void        generateLine(const ci::gl::GlslProgRef & shader);
+        
+        float       mVelocity;
+        float       mTheta;
+        float       mProgress;
+        float       mMaxProgress;
+        bool        mIsDead;
+        bool        mHasExploded;
+        float       mExplosionScale;
+        ci::Vec2f   mCurrentOffset;
+        ci::Vec3f   mInitialPosition;
+        ci::Vec3f   mCurrentPosition;
+        ci::Vec3f   mPointOfExplosion;
+        
+        ci::gl::VaoRef mLineVao;
+        ci::gl::VboRef mLineVbo;
+
     };
     
 }
